03e3741bb4cf1b0ee869d6236212972fd3d066db
“GilsonRavaiani” <gravaiani@yahoo.com.br>
2023-08-23 22:06:30 -0300

    Atualizando script JS

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,25 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+
 const jump = () => {
   mario.classList.add('jump');
 
   setTimeout(() => {
+
     mario.classList.remove('jump');
+
   }, 500);
 
 }
 
 const loop = setInterval((), => {
 
-  const pipeposition = pipe.offsetleft;
+  const pipePosition = pipe.offsetleft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
 
-  /*console.log(pipePosition)*/
+ /*console.log(pipePosition)*/
   
-  if(pipeposition < 120 && pipePosition > 0 && marioPosition < 80) {
+  if(pipePosition < 120 && pipePosition > 0 && marioPosition < 80) {
 
      pipe.style.animation = 'none';
      pipe.style.left = `${pipePosition}px`;
